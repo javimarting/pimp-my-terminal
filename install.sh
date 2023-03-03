d64d0d2c8b628b87b7bffeb74ab9db052b9ec3dc
Javier Martin <jmartin@zerolynx.com>
2023-03-03 10:30:49 +0100

    install.sh changes

--- install.sh
+++ install.sh
@@ -4,10 +4,17 @@ sudo chsh -s /bin/zsh
 wget -O Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
 mkdir -p ~/.fonts
 unzip Hack.zip -d ~/.fonts
-sudo apt install lsd bat zsh-autosuggestions zsh-syntax-highlighting
+wget -O lsd.deb https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
+sudo dpkg -i lsd.deb
+rm lsd.deb
+sudo apt install bat zsh-autosuggestions zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
 sudo mv powerlevel10k /usr/share
-[ -f "~/.zshrc" ] && cp "~/.zshrc" "~/.zshrc_old"
-[ -f "~/.p10k.zsh" ] && cp "~/.p10k.zsh" "~/.p10k_old.zsh"
+if [ -f "~/.zshrc" ]; then
+    cp "~/.zshrc" "~/.zshrc_old"
+fi
+if [ -f "~/.p10k.zsh" ]; then
+    cp "~/.p10k.zsh" "~/.p10k_old.zsh"
+fi
 cp .zshrc "~/.zshrc"
 cp .p10k.zsh "~/.p10k.zsh"
