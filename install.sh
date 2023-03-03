3343a699a20724e5f859e5693a890ba692468ee9
Javier Martin <jmartin@zerolynx.com>
2023-03-03 10:48:24 +0100

    changes in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt install zsh
+sudo apt update && sudo apt install zsh -y
 chsh -s /bin/zsh
 wget -O Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
 mkdir -p ~/.fonts
@@ -10,13 +10,15 @@ sudo dpkg -i lsd.deb
 rm lsd.deb
 sudo apt install zsh-autosuggestions zsh-syntax-highlighting -y
 sudo apt install bat
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
-sudo mv powerlevel10k /usr/share
+if [ ! -d "/usr/share/powerlevel10k" ]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+        sudo mv powerlevel10k /usr/share
+fi
 if [ -f "~/.zshrc" ]; then
 		cp "~/.zshrc" "~/.zshrc_old"
 fi
 if [ -f "~/.p10k.zsh" ]; then
 		cp "~/.p10k.zsh" "~/.p10k_old.zsh"
 fi
-cp .zshrc "~/.zshrc"
-cp .p10k.zsh "~/.p10k.zsh"
+cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
